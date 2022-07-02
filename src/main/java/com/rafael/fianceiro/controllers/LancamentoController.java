39b3afbea480c23fd07a864b0a8f6b6d0b0c370e
= <=>
2022-07-02 12:40:16 -0300

    atualizacao de lancamento e ajuste no cep

--- src/main/java/com/rafael/fianceiro/controllers/LancamentoController.java
+++ src/main/java/com/rafael/fianceiro/controllers/LancamentoController.java
@@ -1,5 +1,7 @@
 package com.rafael.fianceiro.controllers;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
@@ -7,26 +9,32 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.fianceiro.event.RecursoCriadoEvent;
+import com.rafael.fianceiro.exceptionhandler.FinanceiroExceptionHandler.Error;
 import com.rafael.fianceiro.model.Lancamento;
 import com.rafael.fianceiro.model.repositories.LancamentoRepository;
 import com.rafael.fianceiro.model.repositories.filters.LancamentoFilter;
 import com.rafael.fianceiro.model.repositories.projection.ResumoLancamento;
 import com.rafael.fianceiro.service.LancamentoService;
+import com.rafael.fianceiro.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -39,7 +47,10 @@ public class LancamentoController {
 	private LancamentoService lancamentoService;
 	
 	@Autowired
-	private ApplicationEventPublisher publisher;
+	private ApplicationEventPublisher publisher;	
+
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping("/{codigo}")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and hasAuthority('SCOPE_read')")
@@ -64,10 +75,18 @@ public class LancamentoController {
 	
 	@PostMapping
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and hasAuthority('SCOPE_write')")
-	public ResponseEntity<Lancamento> salvar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 		Lancamento lancamentoSalva = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalva.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalva);
+	}
+	
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		List<Error> erros = Arrays.asList(new Error(mensagemUsuario, mensagemDesenvolvedor));
+		return ResponseEntity.badRequest().body(erros);
 	}
 	
 	@DeleteMapping("/{codigo}")
@@ -75,5 +94,16 @@ public class LancamentoController {
 	@PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and hasAuthority('SCOPE_write')")
 	public void deletar(@PathVariable Long codigo) {
 		lancamentoRepository.deleteById(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
+	public ResponseEntity<Lancamento> atualizar(@PathVariable Long codigo, @Valid @RequestBody Lancamento lancamento) {
+		try {
+			Lancamento lancamentoSalvo = lancamentoService.atualizar(codigo, lancamento);
+			return ResponseEntity.status(HttpStatus.OK).body(lancamentoSalvo);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 }
--- src/main/java/com/rafael/fianceiro/service/LancamentoService.java
+++ src/main/java/com/rafael/fianceiro/service/LancamentoService.java
@@ -2,6 +2,7 @@ package com.rafael.fianceiro.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +28,31 @@ public class LancamentoService {
 		}		
 		return lancamentoRepository.save(lancamento);
 	}
+	
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		}
+
+		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		return lancamentoRepository.findById(codigo).orElseThrow(() -> new IllegalArgumentException());
+	}	
 
 }
